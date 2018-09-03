31c7617bef5672f23ac204f030bd8dbed45cae6f
Simos Gerasimou <simos@cs.york.ac.uk>
2018-09-03 22:37:03 +0100

    - added script for testing a NN

---
+++ test_nn.py
@@ -0,0 +1,45 @@
+from utils import load_data, load_model, calculate_prediction_metrics
+import numpy as np
+
+
+def test_model(model_name):
+    """
+    Test a neural network on the MNIST dataset.
+    :return:
+    """
+    #Load MNIST data
+    X_train, Y_train, X_test, Y_test = load_data()
+
+    #Load saved model
+    model = load_model(model_name)
+
+    #Print information about the model
+    print(model.summary())
+
+    #Evaluate the model
+    score = model.evaluate(X_test, Y_test, verbose=0)
+    print('[loss, accuracy] -> ' + str(score))
+
+    #Make predictions
+    Y_pred = model.predict(X_test)
+    #print(Y_pred)
+
+    #Find test and prediction classes
+    Y_test_class = np.argmax(Y_test, axis=1)
+    Y_pred_class = np.argmax(Y_pred, axis=1)
+
+    classifications = np.absolute(Y_test_class - Y_pred_class)
+
+    #Find correct classifications and misclassifications
+    correct_classifications = []
+    incorrect_classifications = []
+    for i in range(1, len(classifications)):
+        if (classifications[i] == 0):
+            correct_classifications.append(i)
+        else:
+            incorrect_classifications.append(i)
+
+    return correct_classifications, incorrect_classifications
+
+
+    print("Testing done!\n")
