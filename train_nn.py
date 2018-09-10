3d61019cecb43bafa9d8ce029396098e1cad4af8
Simos Gerasimou <simos@cs.york.ac.uk>
2018-09-10 13:47:32 +0100

    - fixed arguments bug in train_nn

--- train_nn.py
+++ train_nn.py
@@ -7,18 +7,20 @@ python_version = get_python_version()
 
 def __save_trained_model(model=None, num_hidden=None, num_neuron=None):
     directory = "neural_networks/"
-    model_name = directory + 'mnist_test_model_' + str(num_hidden) + '_' + str(num_neuron)
+    model_name = 'mnist_test_model_' + str(num_hidden) + '_' + str(num_neuron)
     model_filename = directory + model_name + ".json"
-    weights_filename = model_name + ".h5"
+    weights_filename = directory + model_name + ".h5"
 
     # serialize model to JSON
     model_json = model.to_json()
     with open(model_filename, "w") as json_file:
         json_file.write(model_json)
 
+    print("Model saved at: " + model_filename)
+
     # serialize weights to HDF5
     model.save_weights(weights_filename)
-    print("Model saved at: " + model_name)
+    print("Weights saved at: " + weights_filename)
 
     return model_name
 
@@ -31,7 +33,7 @@ def train_model(args, X_train=None, Y_train=None, X_test=None, Y_test=None):
     :return:
     """
 
-    num_hidden = None if args == None else args['hidden']
+    num_hidden = None if args == None else args['layers']
     num_neuron = None if args == None else args['neurons']
 
     if python_version == 2 :
