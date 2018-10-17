491c2445acb4fa030664006bcf86e59ac3dd83ae
hasan ferit <eniserferit@gmail.com>
2018-10-17 22:50:36 +0300

    Change way of perturbations

--- mutate_via_gradient.py
+++ mutate_via_gradient.py
@@ -13,55 +13,75 @@ def mutate(model, X_val, Y_val, layer, dominant_indices, correct_classifications
         grads_for_doms = []
         flatX = [item for sublist in x[0] for item in sublist]
         for dom in dominant_indices:
-            loss = K.mean(model.layers[layer].input[..., dom]) #get_layer('leaky_re_lu_1').output[..., 2])
+            loss = K.mean(model.layers[layer].output[..., dom]) #get_layer('leaky_re_lu_1').output[..., 2])
             grads = normalize(K.gradients(loss, input_tensor)[0])
             iterate = K.function([input_tensor], [loss, grads])
             loss_val, grad_vals = iterate([np.expand_dims(flatX, axis=0)])
             grads_for_doms.append(grad_vals)
 
-        perturbed_x = []
         c1 = 0
         c2 = 0
+
+        min_abs_grad = float('inf')
+        perturbed_x = []
+
+        allAgree = True
         for i in range(len(flatX)):
-            allAgree = True
             for j in range(len(grads_for_doms)):
+                if min_abs_grad < abs(grads_for_doms[j][0][i]):
+                    min_abs_grad = abs(grads_for_doms[j][0][i])
                 if not j == 0 and not np.sign(grads_for_doms[j-1][0][i]) == np.sign(grads_for_doms[j][0][i]):
                     allAgree = False
 
+            if min_abs_grad > d:
+                min_abs_grad = d
+
             if allAgree and grads_for_doms[0][0][i] > 0:
-                perturbed_x.append(min(flatX[i] + d, 1))
+                perturbed_x.append(min(flatX[i] + min_abs_grad, 1))
                 c1 += 1
             elif allAgree and grads_for_doms[0][0][i] < 0:
-                perturbed_x.append(max(flatX[i] - d, 0))
+                perturbed_x.append(max(flatX[i] - min_abs_grad, 0))
                 c2 += 1
             else:
                 perturbed_x.append(flatX[i])
 
         perturbed_set_x.append(perturbed_x)
         perturbed_set_y.append(y)
-        #print(c1)
-        #print(c2)
-        #print('*****')
-        if len(perturbed_set_x) == 10:
-            break
-    
+
     return perturbed_set_x, perturbed_set_y
 
-    '''
+'''
+        for j in range(len(grads_for_doms)): ##
+            perturbed_x = []
+            for i in range(len(flatX)):
+                grad = grads_for_doms[j][0][i]
+                if abs(grad) < d:
+                    c1+=1
+                    perturbed_x.append(flatX[i] + grad)
+                else:
+                    c2+=1
+                    perturbed_x.append(flatX[i] + d)
+
+            perturbed_set_x.append(perturbed_x)
+            perturbed_set_y.append(y)
+'''
+
+'''
     for xv, xp in zip(list(np.array(X_val)[correct_classifications])[100:110], perturbed_set_x):
 
+        xv = np.asarray(xv).reshape(1, 1, 28, 28)
         layer_outs = get_layer_outs(model, xv)
+
         print('BEFORE:')
-        for dom in last_layer_doms:
-            print(layer_outs[target_layer-2][0][0][dom])
+        for dom in dominant_indices:
+            print(layer_outs[layer][0][0][dom])
 
         xp = np.asarray(xp).reshape(1, 1, 28, 28)
         layer_outs = get_layer_outs(model, xp)
         print('AFTER:')
-        for dom in last_layer_doms:
-            print(layer_outs[target_layer-1][0][0][dom])
+        for dom in dominant_indices:
+            print(layer_outs[layer][0][0][dom])
 
         print('========')
-    '''
-    
+'''
 
--- structured_experiments.py
+++ structured_experiments.py
@@ -1,12 +1,13 @@
 import os
 import datetime
 distances   = [0.2, 0.1, 0.05, 0.01]
-labels      = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-model_names = ['mnist_test_model_3_50', 'mnist_test_model_5_30', 'mnist_test_model_8_20']
-arch        = [(3,50), (5,30), (8,20)]
+labels      = [0,3,4,7,9]
+model_names = ['mnist_test_model_5_30', 'mnist_test_model_8_20',
+               'mnist_test_model_6_25']
+arch        = [(5,30), (8,20), (6,25)]
 percents    = [90, 95, 99]
-activations = ['leaky_relu', 'relu']
-techniques  = ['tarantula', 'ochiai', 'random']
+activations = ['leaky_relu']
+techniques  = ['random']
 
 for distance in distances:
     for label in labels:
@@ -14,11 +15,16 @@ for distance in distances:
             for activation in activations:
                 for percent in percents:
                     for tech in techniques:
-                        command = 'python run.py -A ' + tech + ' -N ' + str(arch[model_names.index(model_name)][1]) + ' -HL ' + str(arch[model_names.index(model_name)][0]) + ' -C ' + str(label) + ' -AC ' + activation + ' -D ' + str(distance) + ' -P ' + str(percent) + ' -M ' + model_name + ' -LOG experiment/logfile.log'
+                        command = 'python run.py -A ' + tech + ' -N ' + \
+                        str(arch[model_names.index(model_name)][1]) + ' -HL ' +\
+                        str(arch[model_names.index(model_name)][0]) + ' -C ' +\
+                        str(label) + ' -AC ' + activation + ' -D ' +\
+                        str(distance) + ' -P ' + str(percent) + ' -M ' + \
+                        model_name + ' -LOG experiment/logfile_temp_random.log'
 
                         start = datetime.datetime.now()
                         os.system(command)
                         end = datetime.datetime.now()
-                        logfile = open('experiment/logfile.log','a')
+                        logfile = open('experiment/logfile_temp_random.log','a')
                         logfile.write('Total time (including preperations): ' + str(end-start))
                         logfile.close()
