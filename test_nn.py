ba28399ebc2061b30178290fad13352ea9aa451e
hfeniser <eniserferit@gmail.com>
2018-11-05 15:20:47 +0300

    Make the script able to run standalone

--- test_nn.py
+++ test_nn.py
@@ -1,4 +1,4 @@
-from utils import calculate_prediction_metrics, get_layer_outs, load_data
+from utils import calculate_prediction_metrics, get_layer_outs, load_data, load_model
 import numpy as np
 
 
@@ -48,5 +48,6 @@ def test_model(model, X_test, Y_test):
 
 
 if __name__ == "__main__":
+    model = load_model("neural_networks/mnist_test_model_5_30")
     X_train, Y_train, X_test, Y_test = load_data()
-    test_model("neural_networks/mnist_test_model_5_30", X_test, Y_test)
+    test_model(model, X_test, Y_test)
