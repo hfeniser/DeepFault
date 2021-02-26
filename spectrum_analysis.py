670a6a138901d79f24ecd2c2f631aa8414a90b16
mato <matiasfeduran@gmail.com>
2021-02-26 14:11:34 -0300

    reworked spectrum_analysis

--- spectrum_analysis.py
+++ spectrum_analysis.py
@@ -13,99 +13,74 @@ neighbourings regions.
 
 '''
 
-def tarantula_analysis(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num):
-    '''
-    More information on Tarantula fault localization technique can be found in
-    [1]
-    '''
-    suspicious_neuron_idx = [[] for i in range(1, len(trainable_layers))]
 
+def scores_with_foo(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, foo):
 
     for i in range(len(scores)):
         for j in range(len(scores[i])):
-            score = float(float(num_cf[i][j]) / (num_cf[i][j] + num_uf[i][j])) / (float(num_cf[i][j]) / (num_cf[i][j] + num_uf[i][j]) + float(num_cs[i][j]) / (num_cs[i][j] + num_us[i][j]))
+            score = foo(i, j)
             if np.isnan(score):
                 score = 0
             scores[i][j] = score
 
-    flat_scores = [float(item) for sublist in scores for item in sublist if not
-               math.isnan(float(item))]
+    flat_scores = [float(item) for sublist in scores for item in sublist if not math.isnan(float(item))]
 
-    relevant_vals = sorted(flat_scores, reverse=True)[:suspicious_num]
+    # grab the indexes of the highest suspicious_num scores
+    if suspicious_num >= len(flat_scores):
+        flat_indexes = range(len(flat_scores))
+    else:
+        flat_indexes = np.argpartition(flat_scores, -suspicious_num)[-suspicious_num:]
 
     suspicious_neuron_idx = []
-    for i in range(len(scores)):
-        for j in range(len(scores[i])):
-            if scores[i][j] in relevant_vals:
-                if trainable_layers == None:
-                    suspicious_neuron_idx.append((i,j))
-                else:
-                    suspicious_neuron_idx.append((trainable_layers[i],j))
-            if len(suspicious_neuron_idx) == suspicious_num:
-                break
+    for idx in flat_indexes:
+        # unflatten idx
+        i = 0
+        accum = idx
+        while accum >= len(scores[i]):
+            accum -= len(scores[i])
+            i += 1
+        j = accum
+
+        if trainable_layers is None:
+            suspicious_neuron_idx.append((i, j))
+        else:
+            suspicious_neuron_idx.append((trainable_layers[i], j))
 
     return suspicious_neuron_idx
 
 
+def tarantula_analysis(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num):
+    """
+    More information on Tarantula fault localization technique can be found in
+    [1]
+    """
+    def tarantula(i, j):
+        return float(float(num_cf[i][j]) / (num_cf[i][j] + num_uf[i][j])) / \
+            (float(num_cf[i][j]) / (num_cf[i][j] + num_uf[i][j]) + float(num_cs[i][j]) / (num_cs[i][j] + num_us[i][j]))
 
-def ochiai_analysis(available_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num):
-    '''
+    return scores_with_foo(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, tarantula)
+
+
+def ochiai_analysis(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num):
+    """
     More information on Ochiai fault localization technique can be found in
     [2]
-    '''
+    """
 
-    suspicious_neuron_idx = [[] for i in range(1, len(available_layers))]
+    def ochiai(i, j):
+        float(num_cf[i][j]) / ((num_cf[i][j] + num_uf[i][j]) * (num_cf[i][j] + num_cs[i][j])) **(.5)
 
-    for i in range(len(scores)):
-        for j in range(len(scores[i])):
-            score = float(num_cf[i][j]) / ((num_cf[i][j] + num_uf[i][j]) * (num_cf[i][j] + num_cs[i][j])) **(.5)
-            scores[i][j] = score
-
-    flat_scores = [float(item) for sublist in scores for item in sublist if not
-               math.isnan(float(item))]
-
-    relevant_vals = sorted(flat_scores, reverse=True)[:suspicious_num]
-
-    suspicious_neuron_idx = []
-    for i in range(len(scores)):
-        for j in range(len(scores[i])):
-            if scores[i][j] in relevant_vals:
-                if available_layers == None:
-                    suspicious_neuron_idx.append((i,j))
-                else:
-                    suspicious_neuron_idx.append((available_layers[i],j))
-            if len(suspicious_neuron_idx) == suspicious_num:
-                break
-
-    return suspicious_neuron_idx
+    return scores_with_foo(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, ochiai)
 
 
-def dstar_analysis(available_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, star):
-    '''
+def dstar_analysis(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, star):
+    """
     More information on DStar fault localization technique can be found in
     [3]
-    '''
+    """
 
-    for i in range(len(scores)):
-        for j in range(len(scores[i])):
-            score = float(num_cf[i][j]**star) / (num_cs[i][j] + num_uf[i][j])
-            scores[i][j] = score
+    def dstar(i, j):
+        return float(num_cf[i][j]**star) / (num_cs[i][j] + num_uf[i][j])
 
-    flat_scores = [float(item) for sublist in scores for item in sublist if not
-               math.isnan(float(item))]
-
-    relevant_vals = sorted(flat_scores, reverse=True)[:suspicious_num]
-
-    suspicious_neuron_idx = []
-    for i in range(len(scores)):
-        for j in range(len(scores[i])):
-            if scores[i][j] in relevant_vals:
-                if available_layers == None:
-                    suspicious_neuron_idx.append((i,j))
-                else:
-                    suspicious_neuron_idx.append((available_layers[i],j))
-            if len(suspicious_neuron_idx) == suspicious_num:
-                break
-
-    return suspicious_neuron_idx
+    return scores_with_foo(trainable_layers, scores, num_cf, num_uf, num_cs, num_us, suspicious_num, dstar)
 
